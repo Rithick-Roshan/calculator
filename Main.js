3e443a3d7a0a78cfc7806ee8af0e9dad7d89620e
rithick <143433981+RithickRoshanCholangaThevar@users.noreply.github.com>
2023-12-18 21:46:35 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,21 +2,28 @@ let out=$("#out-element");
 function dispaly(num){
     out.val(out.val()+num);
 }
-
 function calculate()
 {
+    
     try{
         //out.val(eval(out.val()));
-       let exp=out.val();
-        let result=eval(exp);
-        
-        out.val(result);
-        console.log(result);
+        let exp=out.val();
+        let Ex=exp;
+         exp = exp.replace(/\^/g, '**');
+         exp= exp.replace(/√/g,'Math.sqrt');
+         exp= exp.replace(/log/g,'Math.log10');
+         exp= exp.replace(/ln/g,'Math.log');
+         exp= exp.replace(/sin/g,'Math.sin');
+         exp= exp.replace(/cos/g,'Math.cos');
+         exp= exp.replace(/tan/g,'Math.tan');
+         let result=eval(exp);
+         return out.val(result);
+         console.log(result);
     }
     catch(error)
     {
         //alert("invalid");
-        out.val("INVALID")
+        out.val(error);
         
     }
 }
